9e6a5dc3be219024701420691ea85ff67512bf9c
Turtles-are-good <26383@student.macleans.school.nz>
2026-05-30 06:38:06 +0000

    My work

---
+++ Is-it-a-triangle.py
@@ -0,0 +1,12 @@
+def is_triangle(side_a, side_b, side_c):
+    return side_a + side_b < side_c
+
+a = int(input("Enter the smallest side of your shape"))
+b = int(input("Enter the second smallest side of your shape"))
+c = int(input("Enter the largest side of your shape"))
+
+test = is_triangle(a, b, c)
+if is_triangle(a, b, c) :
+    print(a, "+", b, ">", c, "so this shape is a triangle")
+else :
+    print(a, "+", b, "<=", c, "so this shape is not a triangle")
